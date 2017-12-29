f7f80be0a9351674aecf24ae5215700817c57310
Rafael Thayto <rafa.thayto@gmail.com>
2017-12-29 13:02:00 -0200

    Method attack has been implemented

--- app.js
+++ app.js
@@ -10,6 +10,37 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function() {
+            let max = 10;
+            let min = 3;
+            let damange = Math.max(Math.floor(Math.random() * max) + 1, min);
+            this.monsterHealth -= damange;
+
+            if(this.monsterHealth <= 0) {
+                alert('You won!');
+                this.gameIsRunning = false;
+                return;
+            }
+            
+            max = 12;
+            min = 5;
+            damange = Math.max(Math.floor(Math.random() * max) + 1, min);
+            this.playerHealth -= damange;
+
+            if(this.playerHealth <= 0) {
+                alert('You lost!');
+                this.gameIsRunning = false;
+            }
+        },
+        specialAttack: function() {
+
+        },
+        heal: function() {
+
+        },
+        giveUp: function() {
+
         }
     }
 })
